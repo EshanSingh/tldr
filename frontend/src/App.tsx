a2ca4e51ebc2556845fe9716d2af4ee6b25c0d97
EshanSingh <53839727+EshanSingh@users.noreply.github.com>
2025-01-21 14:36:24 -0500

    preparing for frontend deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -28,13 +28,16 @@ const App: React.FC = () => {
     console.log(e.target);
     setLoading(true);
     try {
-      const response = await fetch("http://127.0.0.1:8000/summary", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ input: url, type: type }),
-      });
+      const response = await fetch(
+        "https://beautiful-education-production.up.railway.app/summary/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ input: url, type: type }),
+        }
+      );
 
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
